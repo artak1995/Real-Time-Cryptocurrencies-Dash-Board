9fab6e7f90402e62c11d47f3ce1f05a282a03398
artak1995 <artak1995@gmail.com>
2018-07-25 19:44:17 +0800

    Server API call updated and React displays one cryptocurrency real time

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,40 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import socketIOClient from "socket.io-client";
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      response: false,
+      endpoint: "http://localhost:8000/"
+    };
+  }
+
+  componentDidMount() {
+    const { endpoint } = this.state;
+    const socket = socketIOClient(endpoint);
+    socket.on("FromAPI", data => this.setState({ response: data }));
+    console.log(this.state);
+  }
+
   render() {
+    const { response } = this.state;
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div style={{padding: 20}}>
+        <h1>Cryptocurrency Real Time</h1>
+        {response
+          ? <div>
+            <h2>Bitcon</h2>
+            {/* {response.ticker.price ? */}
+            <h4 style={{color: '#F5AE4B'}}> Price: {response.ticker.price}  </h4>
+            <h6>Volume: {response.ticker.volume}</h6>
+            <h6 style={response.ticker.change < 0? {color: '#D75054'} :{color: '#22776D'}}>{response.ticker.change}</h6>
+            {/* :
+            <h6 style={{color: '#F5AE4B'}}> Price: Loading </h6> */}
+            {/* } */}
+            
+            </div>
+          : <p>Loading...</p>}
       </div>
     );
   }
--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const http = require("http");
 const socketIo = require("socket.io");
 const axios = require("axios");
 
-const port = 3000;
+const port = 8000;
 const index = require("./routes/index");
 
 const app = express();
@@ -12,23 +12,70 @@ app.use(index);
 const server = http.createServer(app);
 const io = socketIo(server);
 
+let interval;
 io.on("connection", socket => {
-  console.log("New client connected"), setInterval(
-    () => getApiAndEmit(socket),
-    1000
-  );
-  socket.on("disconnect", () => console.log("Client disconnected"));
+  console.log("New client connected");
+//   if (interval) {
+//     clearInterval(interval);
+//   }
+  interval = setInterval(() => getApiAndEmit(socket), 5000);
+  socket.on("disconnect", () => {
+    console.log("Client disconnected");
+    clearInterval(interval);
+  });
 });
 
-const getApiAndEmit = async socket => {
+const APIinstance = axios.create({
+    baseURL: 'https://api.cryptonator.com/api/ticker/'
+  });
+  
+const getApiAndEmit =  socket => {
   try {
-    const res = await axios.get(
-      "https://api.cryptonator.com/api/ticker/btc-usd"
-    );
-    socket.emit("FromAPI", res.data);
+    const res =  APIinstance.get('/btc-usd', {
+        maxRedirects: 25,
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+        }
+    })
+        .then(function (response) {
+            // handle success
+            console.log('API CALL');  
+            if (response.data.ticker != undefined)     
+                socket.emit("FromAPI", response.data);
+            console.log(response.data);
+            
+        })
+        .catch(function (error) {
+            // handle error
+            console.log("Erron in API CALL");
+            console.log(error);
+        })
+        .then(function () {
+            // always executed
+            console.log('AFTER API CALL');
+        });
+    // socket.emit("FromAPI", res.data);
+    console.log('END OF TRY BLOCK');
   } catch (error) {
+    console.log('Reached here2');
     console.error(`Error: ${error.code}`);
   }
 };
+
+
+// const res = axios.get('https://api.cryptonator.com/api/ticker/btc-usd')
+//   .then(function (response) {
+//     // handle success
+//     console.log(response.data);
+//   })
+//   .catch(function (error) {
+//     // handle error
+//     console.log(error);
+//   })
+//   .then(function () {
+//     // always executed
+//     console.log('run');
+//   });
 
 server.listen(port, () => console.log(`Listening on port ${port}`));
